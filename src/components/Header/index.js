40c6ad32086e48413ceeac1b0de51ef21dce149f
mmmG <a331998513@gmail.com>
2021-07-24 20:20:01 +0200

    added aria-current

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -41,10 +41,10 @@ const Header = () => {
                     <LogoImg src={Logo} alt="audiophile logo"/>
                 </HeaderLogo>
                 <HeaderNav>
-                    <HeaderNavLink to="/" exact={true} data-active="active" aria-label="link to home page">home</HeaderNavLink>
-                    <HeaderNavLink to="/headphones" exact={true} data-active="active" aria-label="link to headphones page">headphones</HeaderNavLink>
-                    <HeaderNavLink to="/speakers" exact={true} data-active="active" aria-label="link to speakers page">speakers</HeaderNavLink>
-                    <HeaderNavLink to="/earphones" exact={true} data-active="active" aria-label="link to earphones page">earphones</HeaderNavLink>
+                    <HeaderNavLink to="/" exact={true} data-active="active" aria-current={true} aria-label="link to home page">home</HeaderNavLink>
+                    <HeaderNavLink to="/headphones" exact={true} data-active="active" aria-current={true} aria-label="link to headphones page">headphones</HeaderNavLink>
+                    <HeaderNavLink to="/speakers" exact={true} data-active="active" aria-current={true} aria-label="link to speakers page">speakers</HeaderNavLink>
+                    <HeaderNavLink to="/earphones" exact={true} data-active="active" aria-current={true} aria-label="link to earphones page">earphones</HeaderNavLink>
                 </HeaderNav>
                 {
                     isMobile && 
