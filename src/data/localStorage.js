047274900810ee6ec4f09f7a6cc9c7fbce4e2126
mmmG <a331998513@gmail.com>
2021-07-27 01:56:29 +0200

    removed console log

--- src/data/localStorage.js
+++ src/data/localStorage.js
@@ -4,7 +4,6 @@ export const loadState = ()=>{
         if(serializedState === null){
             return undefined;
         }
-        console.log(serializedState)
         return JSON.parse(serializedState);
     } catch (err){
         return undefined;
