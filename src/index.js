8b91fe4589be072f486165572b010202808efea4
mmmG <a331998513@gmail.com>
2021-07-27 01:49:27 +0200

    added localStorage

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import { allReducer } from './reducers';
 import { Provider } from 'react-redux';
+import { loadState, saveState } from './data/localStorage';
 
+
+const persistedState = loadState();
 const store = createStore(
     allReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+);
 
+store.subscribe(throttle(()=>{
+  saveState({
+    products:store.getState().products
+  })
+},1000))
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
