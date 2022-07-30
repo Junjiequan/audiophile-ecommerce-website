e323e2a3d9f8ed11985b4d34ca58fbf9abf1c9e3
mmmG <a331998513@gmail.com>
2022-07-30 16:34:05 +0200

    update api endpoint

--- src/components/CartModal/index.js
+++ src/components/CartModal/index.js
@@ -1,100 +1,113 @@
-import React,{useEffect} from 'react' 
-import { useSelector, useDispatch } from 'react-redux'
-import { toggleModal,addProduct,deleteProduct,resProduct } from '../../actions'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleModal, addProduct, deleteProduct, resProduct } from '../../actions';
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import {
-    CartModalContainer,
-    CartModalWrapper,
-    Modal,
-    FirstRow,
-    Amount,
-    RemoveBtn,
-    ItemsWrapper,
-    Items,
-    ItemImg,
-    ItemInfo,
-    ItemPrice,
-    ItemAmount,
-    ItemDecrement,
-    ItemIncrement,
-    Total,
-    TotalDesc,
-    TotalCost,
-    CheckOutBtn
-} from './CartModalElements'
-
+  CartModalContainer,
+  CartModalWrapper,
+  Modal,
+  FirstRow,
+  Amount,
+  RemoveBtn,
+  ItemsWrapper,
+  Items,
+  ItemImg,
+  ItemInfo,
+  ItemPrice,
+  ItemAmount,
+  ItemDecrement,
+  ItemIncrement,
+  Total,
+  TotalDesc,
+  TotalCost,
+  CheckOutBtn,
+} from './CartModalElements';
 
 const Cart = () => {
-    const isModalToggle = useSelector(state=>state.modalToggle);
-    const products = useSelector(state=>state.products.cartItems);
-    const dispatch = useDispatch();
-    isModalToggle ? disableBodyScroll(document) : enableBodyScroll(document)
-    
-    //click modal outer || click checkout button to close modal
-    useEffect(()=>{
-        document.addEventListener('click',e=>{
-            const isOverlay = e.target.getAttribute('data-container') === 'true'
-            const isCheckout = e.target.getAttribute('data-text') === 'checkout'
-            if(isOverlay||isCheckout){
-                dispatch(toggleModal());
-            }
-        })
-    },[dispatch]);
+  const isModalToggle = useSelector((state) => state.modalToggle);
+  const products = useSelector((state) => state.products.cartItems);
 
-    const displayItems = (item,index)=>{
-        return (
-            <Items key={index}>
-                <ItemImg width="64" height="64" src={require(`../../assets/${item.cartImg}`).default} alt={item.cartImg}/>
-                <ItemInfo>
-                    {item.short}<br />
-                    <ItemPrice>$ {item.price.toLocaleString()}</ItemPrice>
-                </ItemInfo>
-                <ItemAmount>
-                    <ItemDecrement onClick={()=>dispatch(deleteProduct(item))}>-</ItemDecrement>
-                        {item.qty}
-                    <ItemIncrement onClick={()=>dispatch(addProduct(item,1))}>+</ItemIncrement>
-                </ItemAmount>
-            </Items>
-        )
-    }
-    const totalPrice = products.reduce((sum,current)=> sum + current.price * current.qty,0);
+  console.log('-----', products);
+  const dispatch = useDispatch();
+  isModalToggle ? disableBodyScroll(document) : enableBodyScroll(document);
 
-    
+  //click modal outer || click checkout button to close modal
+  useEffect(() => {
+    document.addEventListener('click', (e) => {
+      const isOverlay = e.target.getAttribute('data-container') === 'true';
+      const isCheckout = e.target.getAttribute('data-text') === 'checkout';
+      if (isOverlay || isCheckout) {
+        dispatch(toggleModal());
+      }
+    });
+  }, [dispatch]);
+
+  const displayItems = (item, index) => {
+    console.log(item);
     return (
-        <CartModalContainer data-display={isModalToggle} data-container >
-            <CartModalWrapper data-container >
-                <Modal data-display={isModalToggle} aria-labelledby="cart_button" aria-expanded={isModalToggle} id="cart_modal">
-                    <FirstRow >
-                        <Amount> Cart ({products.length})</Amount>
-                        <RemoveBtn onClick={()=> dispatch(resProduct())}> Remove all</RemoveBtn>
-                    </FirstRow>
+      <Items key={index}>
+        <ItemImg width='64' height='64' src={require(`../../assets/${item.cartImg}`).default} alt={item.cartImg} />
+        <ItemInfo>
+          {item.short}
+          <br />
+          <ItemPrice>$ {item.price.toLocaleString()}</ItemPrice>
+        </ItemInfo>
+        <ItemAmount>
+          <ItemDecrement onClick={() => dispatch(deleteProduct(item))}>-</ItemDecrement>
+          {item.qty}
+          <ItemIncrement onClick={() => dispatch(addProduct(item, 1))}>+</ItemIncrement>
+        </ItemAmount>
+      </Items>
+    );
+  };
+  const totalPrice = products.reduce((sum, current) => sum + current.price * current.qty, 0);
 
-                    <ItemsWrapper>
-                        {
-                            products.length < 1
-                            ? 
-                            <Items style={{justifyContent:'center'}}>
-                                <iframe title="random gif" src="https://giphy.com/embed/nKERd2uhn8hhe" width="180" height="180" style={{pointerEvents:'none',border:'0'}}></iframe>
-                            </Items>
-                            : products.map(displayItems)
-                        }
-                    </ItemsWrapper>
+  return (
+    <CartModalContainer data-display={isModalToggle} data-container>
+      <CartModalWrapper data-container>
+        <Modal data-display={isModalToggle} aria-labelledby='cart_button' aria-expanded={isModalToggle} id='cart_modal'>
+          <FirstRow>
+            <Amount> Cart ({products.length})</Amount>
+            <RemoveBtn onClick={() => dispatch(resProduct())}> Remove all</RemoveBtn>
+          </FirstRow>
 
-                    <Total>
-                        <TotalDesc>Total</TotalDesc>
-                        <TotalCost>$ {totalPrice.toLocaleString()}</TotalCost>
-                    </Total>
-                    {
-                        products.length < 1 
-                        ? <CheckOutBtn data-text="no items in the list" data-black={true} to="/" tabIndex="-1" style={{pointerEvents:'none'}} /> 
-                        : <CheckOutBtn data-text="checkout" data-black={false} to="/checkout" aria-label="to checkout page" /> 
-                    }
-                </Modal>
-            </CartModalWrapper>
-        </CartModalContainer>
-    )
-}
+          <ItemsWrapper>
+            {products.length < 1 ? (
+              <Items style={{ justifyContent: 'center' }}>
+                <iframe
+                  title='random gif'
+                  src='https://giphy.com/embed/nKERd2uhn8hhe'
+                  width='180'
+                  height='180'
+                  style={{ pointerEvents: 'none', border: '0' }}
+                ></iframe>
+              </Items>
+            ) : (
+              products.map(displayItems)
+            )}
+          </ItemsWrapper>
+
+          <Total>
+            <TotalDesc>Total</TotalDesc>
+            <TotalCost>$ {totalPrice.toLocaleString()}</TotalCost>
+          </Total>
+          {products.length < 1 ? (
+            <CheckOutBtn
+              data-text='no items in the list'
+              data-black={true}
+              to='/'
+              tabIndex='-1'
+              style={{ pointerEvents: 'none' }}
+            />
+          ) : (
+            <CheckOutBtn data-text='checkout' data-black={false} to='/checkout' aria-label='to checkout page' />
+          )}
+        </Modal>
+      </CartModalWrapper>
+    </CartModalContainer>
+  );
+};
 
 //React.memo is just used for try out purpose
 
-export default React.memo(Cart)
+export default React.memo(Cart);
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,45 +1,43 @@
-import React ,{useState,useEffect}from 'react'
-import { useLocation } from 'react-router-dom'
-import ProductMain from '../components/Main/ProductMain'
-import Banner from '../components/Banner'
-import ProductNav from '../components/ProductNav'
-import IsLoading from '../IsLoading'
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ProductMain from '../components/Main/ProductMain';
+import Banner from '../components/Banner';
+import ProductNav from '../components/ProductNav';
+import IsLoading from '../IsLoading';
 
 const ProductDetail = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState();
-    const location = useLocation();
-    const path = location.pathname.replace('/product_detail/','');
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  const location = useLocation();
+  const path = location.pathname.replace('/product_detail/', '');
 
-    useEffect(()=>{
-        const fetchJSON = async()=>{
-            try{
-                const resp = await fetch("https://api.jsonbin.io/b/60f2122c0cd33f7437ca192d/5",{});
-                if(!resp.ok){
-                    throw new Error('fetching data failed')
-                };
-                const DATA = await resp.json();
-                const getPageData = DATA.filter(item =>item.id === path ? item : false);
-                setData(getPageData[0]);
-                setIsLoading(false);
-            } catch(err){
-                console.log(err)
-            }
+  useEffect(() => {
+    const fetchJSON = async () => {
+      try {
+        const resp = await fetch('https://api.jsonbin.io/v3/b/62e53f9970a3846ee74c47a4', {});
+
+        if (!resp.ok) {
+          throw new Error('fetching data failed');
         }
-        fetchJSON();
-    },[path]);
+        const DATA = await resp.json();
 
-    return (
-        <main>
-            {
-                isLoading
-                ? <IsLoading />
-                : <ProductMain data={data} />
-            }
-            <ProductNav />
-            <Banner />
-        </main>
-    )
-}
+        const getPageData = DATA.record.filter((item) => (item.id === path ? item : false));
+        setData(getPageData[0]);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchJSON();
+  }, [path]);
 
-export default ProductDetail
+  return (
+    <main>
+      {isLoading ? <IsLoading /> : <ProductMain data={data} />}
+      <ProductNav />
+      <Banner />
+    </main>
+  );
+};
+
+export default ProductDetail;
